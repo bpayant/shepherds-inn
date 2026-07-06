a7df36f607b802de50875e5c11d2a2dff9bc7111
Brian <hocki101@hotmail.com>
2026-07-06 17:10:37 -0500

    Made layouts (primarily buttons) more consistent

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MessageForm } from '../../components/message-form/message-form';
+import { ScheduleVisitForm } from '../../components/schedule-visit-form/schedule-visit-form';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [RouterLink, MessageForm, ScheduleVisitForm],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home {}
+export class Home {
+  showMessageForm = false;
+  showScheduleVisitForm = false;
+}
